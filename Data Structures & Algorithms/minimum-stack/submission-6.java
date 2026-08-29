fea11c083d1624faf79c77574bfa3cc392d3b9c2
Valiyev-Ali <alivaliyev0225@gmail.com>
2026-08-29 14:44:34 -0400

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,38 @@
+class MinStack {
+
+    Deque<Integer> currMin;
+    Deque<Integer> stack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        currMin = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        if (currMin.isEmpty() || val <= currMin.peek()) {
+            currMin.push(val);
+        }
+        else {
+            currMin.push(currMin.peek());
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if (stack.peek() == currMin.peek()) {
+            currMin.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        if (currMin.isEmpty()) {
+            return 0;
+        }
+        return currMin.pop();
+    }
+}
